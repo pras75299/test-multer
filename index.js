4c8412e84357acbcdddfd10e41fc1aaa72c0533b
pras75299 <singh.prashantking@gmail.com>
2024-03-29 00:54:54 +0530

    initial setup of project and install essential dependencies

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.set("view engine", ejs);
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
